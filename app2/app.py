e6e8dd883fa1210e7b05ff814d2bda02be3a46dd
Suriya C U <suriyac.u.99@gmail.com>
2026-04-07 17:27:34 +0530

    Fix string quotes in app.py

--- app2/app.py
+++ app2/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +7,4 @@ def home():
     return "Hello from Suriya"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
