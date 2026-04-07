e23f7be98a56482b7fb2f3de294f58830468fa78
Suriya C U <suriyac.u.99@gmail.com>
2026-04-07 17:03:50 +0530

    Update app.py

--- app2/app.py
+++ app2/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello from Suriya"
+
+if __name__ == "__main__":
+    # host='0.0.0.0' allows the app to be accessible outside the container
+    # port=5000 is the internal container port we will map to 8084
+    app.run(host='0.0.0.0', port=5000)
